d059386df8517763bd8cd3e404fd79634e78c1a7
MIracleyin <miracleyin@live.com>
2022-11-10 03:06:11 +0800

    feat: base auth api

--- apps/keck/src/server/api/auth/mod.rs
+++ apps/keck/src/server/api/auth/mod.rs
@@ -1,50 +1,82 @@
 use axum::{
-    routing::{get, post},
+    async_trait,
+    extract::{FromRequest, RequestParts},
+    response::IntoResponse,
+    routing::post,
     Json, Router,
 };
+use http::StatusCode;
+use jsonwebtoken as jwt;
 use reqwest;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+const FIREBASE_API_KEY: &'static str = "AIzaSyAezKJuZZNcR7XUR9Cm9K7GRMj90DrquQM";
+const SECRET: &[u8] = b"affine-pathfinder";
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct UserInfo {
+    pub name: String,
+    pub email: String,
+    pub avatar_url: String,
+    pub workspaces: Vec<Workspace>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Workspace {
+    pub name: String,
+    pub space_type: String,
+    pub avater_url: String,
+    pub permissions: Vec<String>,
+}
+
+enum WorkspaceType {
+    personal,
+    team,
+}
+
 pub fn auth_apis(router: Router) -> Router {
     let api_handler = Router::new()
-        .route("/signin/:login", post(sign_in_email))
-        .route("/userinfo", get(user_info));
+        .route("/sign_in", post(sign_in_email))
+        .route("/get_user_info", post(get_user_info))
+        .route("/update_workspace", post(update_workspace));
 
     router.nest("/api", api_handler)
 }
 
 #[derive(Deserialize, Serialize)]
-pub struct LoginForm {
+pub struct LoginRequest {
     pub email: String,
     pub password: String,
 }
 
-impl std::fmt::Display for LoginForm {
+pub struct LoginResponse {
+    token: String,
+}
+
+impl std::fmt::Display for LoginRequest {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "email: {}, password: {}", self.email, self.password)
     }
 }
 
 // POST /api/signin
-async fn sign_in_email(Json(login): Json<LoginForm>) -> String {
-    let api_key = "AIzaSyAezKJuZZNcR7XUR9Cm9K7GRMj90DrquQM";
+async fn sign_in_email(Json(login): Json<LoginRequest>) -> impl IntoResponse {
     let mut url = format!(
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={}",
-        api_key
+        FIREBASE_API_KEY
     );
 
-    println!("login: {}", login);
-
-    let mut map = HashMap::new();
-    map.insert("email", "miracleyin@live.com");
-    map.insert("password", "19980804");
+    let login_info = LoginRequest {
+        email: login.email,
+        password: login.password,
+    };
 
     let client = reqwest::Client::new();
     let resp = client
         .post(&url)
         .header("Content-Type", "application/json")
-        .json(&map)
+        .json(&login_info)
         .send()
         .await
         .unwrap()
@@ -52,19 +84,28 @@ async fn sign_in_email(Json(login): Json<LoginForm>) -> String {
         .await
         .unwrap();
 
-    String::from(resp)
+    // expire
+
+    (StatusCode::FOUND, Json(resp))
 }
 
-// POST /api/userinfo
-async fn user_info() -> String {
+// need accessid
+// response payload
+// id: string
+// name: string
+// email: string
+// avatar_url: string (User's avatar )
+// workspace
+// POST /api/get_user_info
+async fn get_user_info1() -> Json<UserInfo> {
     let api_key = "AIzaSyAezKJuZZNcR7XUR9Cm9K7GRMj90DrquQM";
-    let mut url = format!(
+    let url = format!(
         "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key={}",
         api_key
     );
 
     let mut map = HashMap::new();
-    map.insert("idToken", "eyJhbGciOiJSUzI1NiIsImtpZCI6InRCME0yQSJ9.eyJpc3MiOiJodHRwczovL2lkZW50aXR5dG9vbGtpdC5nb29nbGUuY29tLyIsImF1ZCI6InBhdGhmaW5kZXItNTIzOTIiLCJpYXQiOjE2Njc5ODQxNzcsImV4cCI6MTY2OTE5Mzc3NywidXNlcl9pZCI6IlY0eFFLa2drcnhkUFdrU0puVTZXSkluVEZBejIiLCJlbWFpbCI6Im1pcmFjbGV5aW5AbGl2ZS5jb20iLCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQiLCJ2ZXJpZmllZCI6ZmFsc2V9.MYWd7LsOZ_2675iRj7oMbJXN2iO5R7rseuKWmXWgWw6wJqW4d2W-eR6Jh8xuQLlM3hNsFF-o-Kq7POtLdCTicSGOXjtIU388cu2HA-GG0Fx0QN_97HOB44aTply37il10al991pMBM6G_xOM_akH0OIl7p65SyzzYN0p0hYIpDiypvbruww5feqq-oRPpvA7lar332WNapFXyS09E_Ey6PyyQBYr9tS8uzuHKrqhDOPpMCo-ouwH3LMKdgJhzWFXj_qYIPzBrkKS7vzYe8E0OkUIj7oKGsjUOTxCVHmqyYQh2DPqJS0n0EDFRWjrHFo99SvV2qmzNVpAtKg-eVWU8g");
+    map.insert("idToken", "e");
 
     let client = reqwest::Client::new();
     let resp = client
@@ -78,9 +119,87 @@ async fn user_info() -> String {
         .await
         .unwrap();
 
-    String::from(resp)
+    Json(UserInfo {
+        name: "Tom".to_string(),
+        email: "tom@affine.com".to_string(),
+        avatar_url: "https://tom.affine.pro/avatar.png".to_string(),
+        workspaces: vec![
+            Workspace {
+                name: "tom's workspace".to_string(),
+                space_type: "personal".to_string(),
+                avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+                permissions: vec!["write".to_string()],
+            },
+            Workspace {
+                name: "affine workspace".to_string(),
+                space_type: "team".to_string(),
+                avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+                permissions: vec!["read".to_string()],
+            },
+        ],
+    })
 }
 
-async fn query_workspace() -> String {
-    String::from("query workspace")
+async fn get_user_base_info() -> impl IntoResponse {}
+
+async fn get_user_workspace_info() -> impl IntoResponse {}
+
+// POST /api/get_user_info
+// aggrate base info adn workspace info
+async fn get_user_info() -> impl IntoResponse {
+    let userinfo = UserInfo {
+        name: "Tom".to_string(),
+        email: "tom@affine.com".to_string(),
+        avatar_url: "https://tom.affine.pro/avatar.png".to_string(),
+        workspaces: vec![
+            Workspace {
+                name: "tom's workspace".to_string(),
+                space_type: "personal".to_string(),
+                avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+                permissions: vec!["write".to_string()],
+            },
+            Workspace {
+                name: "affine workspace".to_string(),
+                space_type: "team".to_string(),
+                avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+                permissions: vec!["read".to_string()],
+            },
+        ],
+    };
+
+    (StatusCode::FOUND, Json(userinfo))
+}
+
+// request payload
+// workspace_id: string
+// name: Option(string)
+// avatar_url: Option(string)
+// public_read: Option(bool) If true, the workspace can be viewed by anyone who gets the link.
+async fn update_workspace() -> impl IntoResponse {
+    let workspace = Workspace {
+        name: "affine workspace".to_string(),
+        space_type: "team".to_string(),
+        avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+        permissions: vec!["read".to_string()],
+    };
+
+    println!("{:?}", workspace);
+
+    (StatusCode::FOUND, Json(workspace))
+}
+
+#[derive(Debug)]
+pub enum AuthError {
+    Auth,
+    Internal,
+}
+
+impl IntoResponse for AuthError {
+    fn into_response(self) -> axum::response::Response {
+        let (code, msg) = match self {
+            AuthError::Auth => (StatusCode::UNAUTHORIZED, "Unauthorized"),
+            AuthError::Internal => (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error"),
+        };
+        (code, msg).into_response()
+    }
 }
