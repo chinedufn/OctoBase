3a689352c1f3a1db404902968e7d8df881d4a6ba
MIracleyin <miracleyin@live.com>
2022-11-10 18:32:20 +0800

    feat: api

---
+++ apps/keck/src/server/api/auth/error.rs
@@ -0,0 +1,36 @@
+use axum::response::{IntoResponse, Response};
+use http::StatusCode;
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FailResponseBody {
+    code: usize,
+    message: String,
+    errors: Vec<FailResponseError>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FailResponseError {
+    message: String,
+    domain: String,
+    reason: String,
+}
+
+#[derive(Debug)]
+pub enum FirebaseError {
+    Unauthorized,
+    InvaildWorkspace,
+    Internal,
+}
+
+impl IntoResponse for FirebaseError {
+    fn into_response(self) -> Response {
+        let (code, msg) = match self {
+            FirebaseError::Unauthorized => (StatusCode::UNAUTHORIZED, "Unauthorized"),
+            FirebaseError::InvaildWorkspace => (StatusCode::NO_CONTENT, "InvaildWorkspace"),
+            FirebaseError::Internal => (StatusCode::INTERNAL_SERVER_ERROR, "Internal"),
+        };
+        (code, msg).into_response()
+    }
+}
--- apps/keck/src/server/api/auth/mod.rs
+++ apps/keck/src/server/api/auth/mod.rs
@@ -1,18 +1,32 @@
+mod error;
+
+use anyhow::Ok;
 use axum::{
     async_trait,
     extract::{FromRequest, RequestParts},
     response::IntoResponse,
     routing::post,
-    Json, Router,
+    Error, Json, Router,
 };
+use error::FailResponseBody;
 use http::StatusCode;
 use jsonwebtoken as jwt;
 use reqwest;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
+use self::error::FirebaseError;
+
 const FIREBASE_API_KEY: &'static str = "AIzaSyAezKJuZZNcR7XUR9Cm9K7GRMj90DrquQM";
 const SECRET: &[u8] = b"affine-pathfinder";
+
+#[inline]
+fn firebase_auth_url(v: &str, v2: &str) -> String {
+    format!(
+        "https://identitytoolkit.googleapis.com/v1/accounts:{}?key={}",
+        v, v2
+    )
+}
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct UserInfo {
@@ -25,12 +39,13 @@ pub struct UserInfo {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Workspace {
     pub name: String,
-    pub space_type: String,
+    pub workspace_type: WorkspaceType,
     pub avater_url: String,
     pub permissions: Vec<String>,
 }
 
-enum WorkspaceType {
+#[derive(Debug, Serialize, Deserialize)]
+pub enum WorkspaceType {
     personal,
     team,
 }
@@ -38,7 +53,9 @@ enum WorkspaceType {
 pub fn auth_apis(router: Router) -> Router {
     let api_handler = Router::new()
         .route("/sign_in", post(sign_in_email))
-        .route("/get_user_info", post(get_user_info))
+        .route("/config/get_user_info", post(get_user_info))
+        .route("/config/get_user_info1", post(get_user_info1))
+        .route("/config/get_workspace_info", post(get_user_workspace_info))
         .route("/update_workspace", post(update_workspace));
 
     router.nest("/api", api_handler)
@@ -50,8 +67,28 @@ pub struct LoginRequest {
     pub password: String,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct LoginResponse {
-    token: String,
+    kind: String,
+    #[serde(rename = "localId")]
+    user_id: String, // localId, for identification unique user
+    email: String,
+    #[serde(default = "default_display_name")]
+    display_name: String,
+    #[serde(rename = "idToken")]
+    id_token: String, // idToken, for authentication
+    registered: bool,
+}
+
+fn default_display_name() -> String {
+    String::from("")
+}
+
+impl IntoResponse for LoginResponse {
+    fn into_response(self) -> axum::response::Response {
+        let body = serde_json::to_string(&self).unwrap();
+        (StatusCode::OK, body).into_response()
+    }
 }
 
 impl std::fmt::Display for LoginRequest {
@@ -61,7 +98,7 @@ impl std::fmt::Display for LoginRequest {
 }
 
 // POST /api/signin
-async fn sign_in_email(Json(login): Json<LoginRequest>) -> impl IntoResponse {
+async fn sign_in_email(Json(login): Json<LoginRequest>) -> Json<LoginResponse> {
     let mut url = format!(
         "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={}",
         FIREBASE_API_KEY
@@ -73,6 +110,7 @@ async fn sign_in_email(Json(login): Json<LoginRequest>) -> impl IntoResponse {
     };
 
     let client = reqwest::Client::new();
+
     let resp = client
         .post(&url)
         .header("Content-Type", "application/json")
@@ -84,9 +122,8 @@ async fn sign_in_email(Json(login): Json<LoginRequest>) -> impl IntoResponse {
         .await
         .unwrap();
 
-    // expire
-
-    (StatusCode::FOUND, Json(resp))
+    let login_response: LoginResponse = serde_json::from_str(&resp).unwrap();
+    Json(login_response)
 }
 
 // need accessid
@@ -119,30 +156,32 @@ async fn get_user_info1() -> Json<UserInfo> {
         .await
         .unwrap();
 
+    let Json(workspace) = get_user_workspace_info().await;
+
     Json(UserInfo {
         name: "Tom".to_string(),
         email: "tom@affine.com".to_string(),
         avatar_url: "https://tom.affine.pro/avatar.png".to_string(),
-        workspaces: vec![
-            Workspace {
-                name: "tom's workspace".to_string(),
-                space_type: "personal".to_string(),
-                avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
-                permissions: vec!["write".to_string()],
-            },
-            Workspace {
-                name: "affine workspace".to_string(),
-                space_type: "team".to_string(),
-                avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
-                permissions: vec!["read".to_string()],
-            },
-        ],
+        workspaces: workspace,
     })
 }
 
-async fn get_user_base_info() -> impl IntoResponse {}
-
-async fn get_user_workspace_info() -> impl IntoResponse {}
+async fn get_user_workspace_info() -> Json<Vec<Workspace>> {
+    Json(vec![
+        Workspace {
+            name: "tom's workspace".to_string(),
+            workspace_type: WorkspaceType::personal,
+            avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+            permissions: vec!["write".to_string()],
+        },
+        Workspace {
+            name: "affine workspace".to_string(),
+            workspace_type: WorkspaceType::team,
+            avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
+            permissions: vec!["read".to_string()],
+        },
+    ])
+}
 
 // POST /api/get_user_info
 // aggrate base info adn workspace info
@@ -154,18 +193,20 @@ async fn get_user_info() -> impl IntoResponse {
         workspaces: vec![
             Workspace {
                 name: "tom's workspace".to_string(),
-                space_type: "personal".to_string(),
+                workspace_type: WorkspaceType::personal,
                 avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
                 permissions: vec!["write".to_string()],
             },
             Workspace {
                 name: "affine workspace".to_string(),
-                space_type: "team".to_string(),
+                workspace_type: WorkspaceType::team,
                 avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
                 permissions: vec!["read".to_string()],
             },
         ],
     };
+
+    println!("{:?}", userinfo);
 
     (StatusCode::FOUND, Json(userinfo))
 }
@@ -178,7 +219,7 @@ async fn get_user_info() -> impl IntoResponse {
 async fn update_workspace() -> impl IntoResponse {
     let workspace = Workspace {
         name: "affine workspace".to_string(),
-        space_type: "team".to_string(),
+        workspace_type: WorkspaceType::team,
         avater_url: "https://workspace.affine.pro/avatar.png".to_string(),
         permissions: vec!["read".to_string()],
     };
@@ -186,20 +227,4 @@ async fn update_workspace() -> impl IntoResponse {
     println!("{:?}", workspace);
 
     (StatusCode::FOUND, Json(workspace))
-}
-
-#[derive(Debug)]
-pub enum AuthError {
-    Auth,
-    Internal,
-}
-
-impl IntoResponse for AuthError {
-    fn into_response(self) -> axum::response::Response {
-        let (code, msg) = match self {
-            AuthError::Auth => (StatusCode::UNAUTHORIZED, "Unauthorized"),
-            AuthError::Internal => (StatusCode::INTERNAL_SERVER_ERROR, "Internal Server Error"),
-        };
-        (code, msg).into_response()
-    }
 }
